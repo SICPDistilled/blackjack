361d9deb4545f65db284b17253bb3ff295038a45
Stuart Terrett <shterrett@gmail.com>
2015-03-18 06:32:39 -0400

    Scores Ace properly

--- test/blackjack/hand_test.clj
+++ test/blackjack/hand_test.clj
@@ -13,3 +13,13 @@
     (testing "is 2 shuffled decks"
       (is (= (set two-decks)
              (set (concat full-deck full-deck)))))))
+
+(deftest test-hand-total
+  (testing "sums hand when just number cards"
+    (is (= 17 (total [{:rank 5} {:rank 2} {:rank 10}]))))
+  (testing "face cards are 10"
+    (is (= 30 (total [{:rank :J} {:rank :Q} {:rank :K}]))))
+  (testing "ace is 11 when hand is small"
+    (is (= 15 (total [{:rank :A} {:rank 4}]))))
+  (testing "ace is 1 when hand is large"
+    (is (= 21 (total [{:rank :K} {:rank :A} {:rank 10}])))))
